b183c36726250bddc0aa484c40b7659d756e0b8e
Ennea <Ennea@users.noreply.github.com>
2022-02-01 22:25:02 +0100

    bump version to 1.1.1

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('app', () => ({
-        VERSION: '1.1',
+        VERSION: '1.1.1',
 
         // raw data from the backend
         bannerTypes: {},
